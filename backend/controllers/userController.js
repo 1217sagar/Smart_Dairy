3c7edb9d3f0c3c07b343ce239a8ae3ce77b98edc
1217sagar <nains4104@gmail.com>
2023-09-20 23:43:42 +0530

    backend authentication completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,19 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import User from '../models/usermodel.js'
+import User from '../models/usermodel.js';
+import generateToken from '../utils/generateTokens.js';
 
 // @description: Auth user & get token
-// @route: GET /api/users/login
+// @route: POST /api/users/login
 // @access: Public
 const authUser = asyncHandler(async(req, res) => {
   const {email, password} = req.body;
 
   const user = await User.findOne({email});
 
-  if(user){
-    res.json({
+  if(user && (await user.matchPassword(password))){ 
+    generateToken(res, user._id)
+
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -19,38 +22,104 @@ const authUser = asyncHandler(async(req, res) => {
   }
   else{
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error('Invalid Email or Password');
   }
-
-  res.send('auth user')
 })
 
 // @description: Register user
 // @route: POST /api/users
 // @access: Public
 const registerUser = asyncHandler(async(req, res) => {
-  res.send('register user')
+  const {name, email, password} = req.body;
+
+  const userExists = await User.findOne({email});
+
+  if(userExists){
+    res.status(400);
+    throw new Error('User already Exist. Kindly login!');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user){
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  }
+  else{
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 })  
 
 // @description: Logout user/clear cookies
 // @route: POST /api/users/logout
 // @access: Private
 const logoutUser = asyncHandler(async(req, res) => {
-  res.send('logout user')
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+
+  res.status(200).json({message: 'Logout Successful'});
 })
 
 // @description: Get user Profile
 // @route: GET /api/users/profile
 // @access: Private
 const getUserProfile = asyncHandler(async(req, res) => {
-  res.send('get user profile')
+  const user = await User.findById(req.user._id);
+  
+  if(user){
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  }
+  else{
+    res.status(400);
+    throw new Error('User not found');
+  }
 })
 
 // @description: Update user Profile
 // @route: PUT /api/users/profile
 // @access: Private
 const updateUserProfile = asyncHandler(async(req, res) => {
-  res.send('update user profile')
+  const user = await User.findById(req.user._id);
+
+  if(user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if(req.body.password){
+      user.password = req.body.password;
+    }
+
+    const updateUser = await user.save();
+
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin
+    })    
+  }
+  else{
+    res.status(400);
+    throw new Error('User not found');
+  }
 })
 
 // @description: Get users
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ dotenv.config();
 import connectDB from './config/db.js';
 import ProductRoutes from './routes/ProductRoutes.js';
 import userRoutes from './routes/userRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 connectDB();
 
@@ -15,6 +16,9 @@ const app = express();
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running good ...');
